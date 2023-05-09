0506dfbfae2e19175ce1cd832d1e81900a670192
jlh425 <105890316+jlh425@users.noreply.github.com>
2023-05-08 20:51:08 -0600

    ball and works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
@@ -80,11 +80,11 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          The presented projects offer real-world examples of my skills and experience.
+           Each project is concisely described with links to code repositories 
+           and live demos, serving as a testament to my proficiency in solving 
+           complex problems, utilizing various technologies, and managing projects
+            effectively.
         </motion.p>
       </div>
 
